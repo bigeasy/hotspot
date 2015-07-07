885eb374363a0aab4245265c472716c90bf94e82
Alan Gutierrez <alan@prettyrobots.com>
2015-07-07 09:44:20 -0400

    Sketch of Hotspot.

--- hotspot.js
+++ hotspot.js
@@ -1,1 +1,24 @@
-module.exports = 1
+function Hotspot () {
+}
+! function (definition) {
+    /* istanbul ignore next */
+    if (typeof module === 'object') module.exports = definition()
+    else if (typeof window !== 'undefined') window.cadence = definition()
+    else if (typeof define === 'function') define(definition)
+} (function () {
+    var exports = {}
+
+    function hotspot () {
+        var I = arguments.length
+        var vargs = new Array(I)
+        for (var i = 0; i < I; i++) {
+            vargs[i] = arguments[i]
+        }
+        return _hotspot(vargs)
+    }
+
+    function _hotspot (vargs) {
+    }
+
+    return hotspot
+})
--- t/hotspot/hotspot.t.js
+++ t/hotspot/hotspot.t.js
@@ -2,4 +2,29 @@ require('proof')(1, require('cadence/redux')(prove))
 
 function prove (async, assert) {
     assert(require('../..'), 'require')
+
+    function echo (value, callback) {
+        callback(null, value)
+    }
+
+    function Program (object) {
+        this.object = object
+    }
+
+    Program.prototype.execute = hotspot(function (define) {
+        define(function (async) {
+            echo(1, async())
+        }, function (async, object, value) {
+            this.object.value = value
+            return [ value ]
+        })
+    })
+
+    async(function () {
+        var object = { value: null }
+        var program = new Program(object)
+        program.execute(async())
+    }, function (value) {
+        assert(value, 1, 'minimal')
+    })
 }
