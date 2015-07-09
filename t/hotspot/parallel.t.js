905f6aa1f581fd8edf3ded32856ce40058c511a0
Alan Gutierrez <alan@prettyrobots.com>
2015-07-09 07:41:28 -0400

    Test parallel gathering.

---
+++ t/hotspot/parallel.t.js
@@ -0,0 +1,25 @@
+require('proof')(2, prove)
+
+function prove (assert) {
+    var hotspot = require('../..')
+
+    hotspot(function (async) {
+        var first = async()
+        var second = async()
+        second(null, 3)
+        first(null, 1, 2)
+    })(function (error, one, two, three) {
+        if (error) throw error
+        assert([ one, two, three ], [ 1, 2, 3 ], 'parallel')
+    })
+
+    hotspot(function (async) {
+        var first = async()
+        var second = async()
+        second(null, 3)
+        first(null, 1, 2)
+    }, [])(function (error, array) {
+        if (error) throw error
+        assert(array, [ 1, 2, 3 ], 'parallel gather')
+    })
+}
