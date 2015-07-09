8473a5d015818f7f9e1e500bc806156fe465a0c7
Alan Gutierrez <alan@prettyrobots.com>
2015-07-09 07:19:06 -0400

    Test returned values.

---
+++ t/hotspot/return.t.js
@@ -0,0 +1,18 @@
+require('proof')(2, prove)
+
+function prove (assert) {
+    var hotspot = require('../..')
+    hotspot(function () {
+        return 1
+    })(function (error, result) {
+        if (error) throw error
+        assert(result, 1, 'returned')
+    })
+
+    hotspot(function () {
+        return [ [ 1 ] ]
+    })(function (error, result) {
+        if (error) throw error
+        assert(result, [ 1 ], 'returned array')
+    })
+}
