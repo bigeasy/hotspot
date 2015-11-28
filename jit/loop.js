0348696660dda2e6bf8f6b8d2ab6bc8034ba9116
Alan Gutierrez <alan@prettyrobots.com>
2015-11-27 22:45:21 -0600

    Update `jit/loop.js` for new break and continue.

--- jit/loop.js
+++ jit/loop.js
@@ -3,8 +3,8 @@ function main () {
     var abend = require('abend')
 
     var f = hotspot(function (async, count, stop) {
-        if (count < stop) return [ async.repeat(), count + 1, stop ]
-        else return [ async.done(), count ]
+        if (count < stop) return [ async.continue, count + 1, stop ]
+        else return [ async.break, count ]
     })
 
     for (var i = 0; i < 100000; i++) {
